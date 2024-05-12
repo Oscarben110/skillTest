fe6cbe614f6446285f1a4258aa0837981953cbd1
o-ben <o.ben@gbh.com.do>
2024-05-12 19:22:36 -0400

    Adding reporting and video recording.

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,24 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  video: true,
+  videoCompression: 32,
+  reporter: 'cypress-mochawesome-reporter',
+  "reporterOptions": {
+    "html": false,
+    "saveJson": true,
+    "charts": true,
+    "reportDir": "cypress/report/mocha",
+    "reportTitle": "Sales page report",
+    "reportFilename": "report",
+    "reportPageTitle": "Sales Page",
+    "overwrite": true
+  },
   e2e: {
     baseUrl: 'https://automationexercise.com/',
     testIsolation: false,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
